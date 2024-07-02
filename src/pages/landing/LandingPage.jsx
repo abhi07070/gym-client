f9e864f09f65a1c03618c092c415fdd40f768320
Davinder Kumar <abhiverma97819@gmail.com>
2024-07-03 00:32:59 +0530

    added tailwind css

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
 };
 
 export default LandingPage;
